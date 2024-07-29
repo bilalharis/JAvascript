10eaafeabab217bf3730dc4d51d1c86b243a47a7
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 08:16:56 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//=== check equality and datatype too
+
+//always prefer comparision with same datatypes 
+//opposite datatype comparision  is avoided mostly for clean code as it make many confusions
