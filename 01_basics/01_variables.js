53cba19992ea740bb0430065bdc4fc5ac3f3e1a1
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 07:07:27 +0000

    Varaible in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "haris@google.com"
+var accountPassword = "12345"
+accountCity = "Gujranwala"
+let accountState;
+
+// accountId = 2
+console.log(accountId);
+accountEmail ="bilal@gmail.com"
+accountPassword = "6746"
+accountCity = "lahore"
+
+// dont use var because its not useful in scope 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
