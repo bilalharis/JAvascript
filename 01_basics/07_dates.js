bdcd08ade36a43c368948810ce809afe5ab3f1d1
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 11:35:04 +0000

    date in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2001, 3, 26)
+// console.log(myCreatedDate.toDateString());
+// console.log(Math.floor(Date.now()/1000));  //date in seconds
+let newDate = new Date()
+console.log(newDate.getDay());
