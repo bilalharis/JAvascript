c2cccfbb40e99e3100b0fcf8a1858768dad065dc
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 10:28:30 +0000

    number and math methods

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+
+//************ Numbers Method *************/
+const score = 400
+// console.log(score);
+const balance = new Number (100)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumer = 23.8966
+// console.log(otherNumer.toPrecision(2));
+// console.log(otherNumer.toPrecision(3));
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-PK'));
+
+
+
+//********** Maths Methods ************/
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+
+console.log(Math.random());
+console.log(Math.floor((Math.random()*6)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
