4b857639333525e0416661f53b09a8d40bd7668b
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 07:30:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version 
+
+//alert(3+3) we are using Node.js not, browser
+
+console.log(3+
+    3)      //code readability should e high
+console.log();
+
+
+
+// ********Primitive data types*********
+//number => 2 to power 53
+//bigint => for large number 
+//string +> "" 
+//boolean => true/false
+//null => standalone value   null is object when check through typeof
+//undefiend => value not assign
+//symbol   => unique
+
+
+
+console.log(typeof "haris");
+console.log(typeof undefined); //undefiend
+console.log(typeof null);    //object 
