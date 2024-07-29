28c914026895a9ac5b865a402937d24235360ac8
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 08:33:47 +0000

    datatypes summary

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,32 @@
+// ********* primitive datatypes ***********
+
+// 7 types= string, Number, Boolean, null, undefined, symbol, BigInt   (give copy)
+
+//********** Reference or Non-Primitive datatypes ************
+// 3 types
+//Arrays, Objects, Functions   (give reference) 
+
+
+const score = 100    //number
+const scoreValue = 100.3  //##
+
+const isLoggedIn = false //boolean
+const outsideTemp = null   //null
+let userEmail; //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')  // return value is always different for both symbol
+
+const bigNumber = 12563876587746n //BigInt
+
+const heroes = ["hero1", "hero2", "hero3"]  //arrays
+
+let myObj = {
+    name: "haris",    //object
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("hello world");    //function
+}
+
