b36401d6523ef647d5cef43d47d68d56dda0ac17
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-07-29 08:04:57 +0000

    Conversion of data types and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33"
+
+
+let valueInNUmber = Number(score)
+console.log(typeof valueInNUmber);
+
+
+// "33"  => 33
+// "33abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn = 1
+let boleeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boleeanIsLoggedIn);
+
+// 1 => true;  0 => false
+//"" => false
+// "haris" => true
+
+
+
+// *************Operations**************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " haris"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
