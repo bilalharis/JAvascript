46f121b54802edb7ce039705c0ac4675102caeed
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-08-02 07:52:52 +0000

    for loop with break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,57 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best number");
+        
+    }
+   // console.log(element);
+    
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+  //  console.log(`outer loop value is ${i}`);
+
+    for (let j = 0; j <= 10; j++) {
+      //  console.log(i + '*' + j + '=' + i*j);
+        
+        
+    }
+    
+    
+}
+
+
+let myArray = ["haris", "bilal", "ali"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+   // console.log(element);
+    
+    
+}
+
+//break and continue
+
+// for(let i = 1; i<= 20; i++){
+
+//     if (i == 5) {
+//         console.log("5 is detected");
+//         break
+        
+//     }
+//     console.log(`value of i is: ${i}`);
+    
+// }
+for(let i = 1; i<= 20; i++){
+
+    if (i == 5) {
+        console.log("5 is detected");
+        continue
+        
+    }
+    console.log(`value of i is: ${i}`);
+    
+}
