c501aa46c6ee666c3c5e686f8d1eff946d05fa63
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-08-02 09:52:06 +0000

    foe in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,21 @@
+// for in loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+
+for (const key in myObject) {
+   //console.log(`${key} shortcut is for ${myObject[key]}`);
+   
+}
+
+const programming = ["java", "c++", "react", "angular"]
+
+for(const key in programming){
+        console.log(`${key} of ${programming[key]}`);
+        
+}
