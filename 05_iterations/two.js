f66eb5756e36c97310daf4f3b69bdc2bdc889046
bilalharis <148871588+bilalharis@users.noreply.github.com>
2024-08-02 08:11:27 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+
+// index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is: ${index}`);
+//        index = index + 1
+// }
+
+let myArray = ["haris", "bilal", "ali"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`the value is: ${myArray[arr]}`);
+
+    arr = arr +1
+    
+}
+
+let score = 1;
+
+do {
+    console.log(`value is ${score}`);
+    score++
+} while (score <= 10);
